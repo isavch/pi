228aa14df977d896c8ad0517995ea07b1e897f84
Ivan Savchenko <ivan_savchenko@epam.com>
2018-06-01 11:26:17 +0300

    change pin

--- temperature.js
+++ temperature.js
@@ -2,7 +2,7 @@ var sensor = require('node-dht-sensor');
 
 console.log('Starting....')
 
-sensor.read(11, 3, function(err, temperature, humidity) {
+sensor.read(11, 2, function(err, temperature, humidity) {
     if (err) {
         console.log('Error!', err.message)
         process.exit();
