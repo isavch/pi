38d346a7b64880330775e56e256ad4699f171f74
Ivan Savchenko <ivan_savchenko@epam.com>
2018-06-01 11:16:21 +0300

    fix

--- temperature.js
+++ temperature.js
@@ -2,11 +2,24 @@ var sensor = require('node-dht-sensor');
 
 console.log('Starting....')
 
-sensor.read(22, 2, function(err, temperature, humidity) {
-    if (err) {
-        console.log('Error!', err.message)
-        process.exit();
-    }
+// sensor.read(22, 2, function(err, temperature, humidity) {
+//     if (err) {
+//         console.log('Error!', err.message)
+//         process.exit();
+//     }
 
-    console.log('temp: ', temperature.toFixed(1),'°C, ', 'humidity: ', humidity.toFixed(1), '%');
-});
+//     console.log('temp: ', temperature.toFixed(1),'°C, ', 'humidity: ', humidity.toFixed(1), '%');
+// });
+
+var rpiDhtSensor = require('rpi-dht-sensor');
+
+var dht = new rpiDhtSensor.DHT22(2);
+
+function read () {
+  var readout = dht.read();
+
+    console.log('Temperature: ' + readout.temperature.toFixed(2) + 'C, ' +
+        'humidity: ' + readout.humidity.toFixed(2) + '%');
+    setTimeout(read, 5000);
+}
+read();
