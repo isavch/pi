903599ff0b4ea40074cff4cb34dca8c846873b1a
Ivan Savchenko <ivan_savchenko@epam.com>
2018-06-01 11:31:20 +0300

    init check

--- temperature.js
+++ temperature.js
@@ -2,7 +2,12 @@ var sensor = require('node-dht-sensor');
 
 console.log('Starting....')
 
-sensor.read(11, 2, function(err, temperature, humidity) {
+if (!sensor.initialize(11, 3)) {
+    console.warn('Failed to initialize sensor');
+    process.exit(1);
+}
+
+sensor.read(11, 3, function(err, temperature, humidity) {
     if (err) {
         console.log('Error!', err.message)
         process.exit();
